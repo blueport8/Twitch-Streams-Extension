e68c29b39da7e0037fd2ae6731e926a4e6679b43
mslaby <mateusz.s00@outlook.com>
2017-08-25 13:29:59 +0200

    implemented stream information frame

--- background/stream_wrapper.js
+++ background/stream_wrapper.js
@@ -1,22 +1,40 @@
 function getLiveStream(streamerData){
-    var liveStreamHTML = getLiveStreamLinkBeginning(streamerData.name) + getLiveStreamHeader();
-    liveStreamHTML += streamerData.name;
-    liveStreamHTML += getLiveStreamEnding() + getLiveStreamLinkEnding();
-    return liveStreamHTML;
+    return `
+        <a href="#" class="stream_link">
+            ${buildStreamFrame(streamerData)}
+        </a>
+    `;
 }
 
-function getLiveStreamLinkBeginning(channelName) {
-    return "<a href=\"#\" class=\"stream_link\">";
+function buildStreamFrame(streamerData) {
+    var imageUrl = streamerData.video_banner;
+    return `
+        <div class="stream_frame">
+            <div class="live_stream_information_frame">
+                ${buildStreamInformationFrame(streamerData)}
+            </div>
+            <div class="live_stream_image">
+                ${buildStreamImageFrame(imageUrl)}
+            </div>
+        </div>
+    `;
+    getLiveStreamHeader();
+    streamerData.name;
+
+    frameHtml += "</div>";
+    return frameHtml;
 }
 
-function getLiveStreamLinkEnding() {
-    return "</a>";
+function buildStreamInformationFrame(streamerData) {
+    return `
+        <div class="channel_name">${streamerData.name}</div>
+        <div class="channel_game">${streamerData.game}</div>
+        <div class="channel_title">${streamerData.status}</div>
+    `
 }
 
-function getLiveStreamHeader() {
-    return "<div class=\"channel_name\">";
-}
-
-function getLiveStreamEnding() {
-    return "</div>";
+function buildStreamImageFrame(imageUrl) {
+    return `
+        <img src="${imageUrl}" class="channel_image"></img>
+    `
 }
